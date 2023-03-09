30d96a71c94c8770bc01d0c1787a818e27b39de2
Eric Zheng <erryzheng@gmail.com>
2023-03-09 15:07:45 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,16 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Eric Sitao Zheng
  */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n % 2 == 0) {
+            n = n/2;
+            return n;
+        }  else {
+            n = n*3+1;
+            return n;
         }
     }
 
